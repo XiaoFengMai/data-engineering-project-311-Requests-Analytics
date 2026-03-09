8af7b4d081e8f5f05c0e5461bf909893555adf80
XiaoFengMai <113124825+XiaoFengMai@users.noreply.github.com>
2026-03-09 12:24:32 -0400

    Update complaints_by_type.sql

--- data build tool/models/marts/complaints_by_type.sql
+++ data build tool/models/marts/complaints_by_type.sql
@@ -1,1 +1,8 @@
+# this sql query is used to power looker dashboard charts
+SELECT
+    complaint_type,
+    COUNT(*) AS total_requests
+FROM {{ ref('stg_311_requests') }}
+GROUP BY complaint_type
+ORDER BY total_requests DESC
 
