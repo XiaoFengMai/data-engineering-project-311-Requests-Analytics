fcc8c43913fdedc26141449e7c279b5cad989f73
XiaoFengMai <113124825+XiaoFengMai@users.noreply.github.com>
2026-03-09 12:32:15 -0400

    Update fct_311_complaints.sql

--- data build tool/models/marts/fct_311_complaints.sql
+++ data build tool/models/marts/fct_311_complaints.sql
@@ -2,5 +2,5 @@ SELECT
     borough,
     complaint_type,
     COUNT(*) AS complaint_count
-FROM {{ ref('stg_311_requests.sql') }}
+FROM {{ ref('stg_311_requests') }}
 GROUP BY borough, complaint_type
