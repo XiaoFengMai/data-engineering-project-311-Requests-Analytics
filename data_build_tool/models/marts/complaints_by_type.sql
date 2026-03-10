f53e646602e87f8ed838dfe9eeee09ffbc9b7a8d
XiaoFengMai <113124825+XiaoFengMai@users.noreply.github.com>
2026-03-10 09:55:27 -0400

    Create complaints_by_type.sql

---
+++ data_build_tool/models/marts/complaints_by_type.sql
@@ -0,0 +1,7 @@
+# this query filters complaint types displayed within looker dashboard charts
+SELECT
+    complaint_type,
+    COUNT(*) AS total_requests
+FROM {{ ref('stg_311_cleaned') }}
+GROUP BY complaint_type
+ORDER BY total_requests DESC
