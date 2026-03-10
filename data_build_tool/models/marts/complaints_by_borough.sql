852fa1329dd1a9c0493ab58702de5208d4313ab1
XiaoFengMai <113124825+XiaoFengMai@users.noreply.github.com>
2026-03-10 13:25:15 -0400

    Update complaints_by_borough.sql

--- data_build_tool/models/marts/complaints_by_borough.sql
+++ data_build_tool/models/marts/complaints_by_borough.sql
@@ -1,8 +1,10 @@
-# filters out complaints by borough within dashboard charts
+# aggregates 311 service requests by borough within dashboard charts
+# answers "how many 311 complaints were filed in each NYC borough, from most to least?
 
 SELECT
-    borough,
-    COUNT(*) AS total_requests
-FROM {{ ref('stg_311_requests') }}
-GROUP BY borough
-ORDER BY total_requests DESC
+    borough,                        # selects borough colum
+    COUNT(*) AS total_requests            # counts every row and labels that as total_requests 
+FROM {{ ref('stg_311_requests') }}            # ref is a dbt function for referening another model in your project, in this case
+                                                # it references the staging model, stg_311 requests, using ref builds model in right order
+GROUP BY borough                        # groups all rows by borough value so the count(*) is calculated per borough
+ORDER BY total_requests DESC                # sorts results from highest to lowest request count, so the busiest borough appears first
