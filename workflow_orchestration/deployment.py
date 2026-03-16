d581f5aa20048c830008b7048807a6422ecf4a23
XiaoFengMai <113124825+XiaoFengMai@users.noreply.github.com>
2026-03-16 13:58:20 -0400

    Update deployment.py

--- workflow_orchestration/deployment.py
+++ workflow_orchestration/deployment.py
@@ -1,10 +1,12 @@
 # this schedule runs the pipeline daily at 2am
 
+# imports the pipeline flow from nyc_311_flow
 from nyc_311_flow import nyc_311_flow
 
+# standard python guard that ensures this deployment code only runs when the file is executed directly
 if __name__ == "__main__":
-    nyc_311_flow.deploy(
-        name="nyc-311-daily",
-        work_pool_name="default-agent-pool",
-        cron="0 2 * * *"
+    nyc_311_flow.deploy(                    # calls prefect's built-in deploy method, which registers and configures it with prefect server for scheduled execution
+        name="nyc-311-daily",                    # names the deployment, this is what is displayed in the prefect UI dashboard
+        work_pool_name="default-agent-pool",       # tells prefect which work pool should pick up and execute this flow, agent is a process that listens for scheduled runs and executes them         
+        cron="0 2 * * *"                          # sets the schedule using cron syntax, runs everday at 2am
     )
