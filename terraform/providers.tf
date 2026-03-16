a4ed11b25a7f1cdee4a0f3d3d9ab09893d1d28ed
XiaoFengMai <113124825+XiaoFengMai@users.noreply.github.com>
2026-03-16 09:33:26 -0400

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,15 +1,16 @@
-# cloud provider configuration
+# configures google cloud as the cloud provider
 
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "~> 5.0"
+
+terraform {                    # opens the main terraform configuration block used to configure behavior and requirements
+  required_providers {                # declares which external providers this project depends on
+    google = {                              # provider is google
+      source = "hashicorp/google"                    # tells terraform to downlaod the provider from official google provider by hashicorp
+      version = "~> 5.0"                    # selects any version 5.0 to the highest 5. version
     }
   }
 }
 
-provider "google" {
-  project = var.project_id
-  region = var.region
+provider "google" {              # sets up credentials and defaults for all google resources in your project
+  project = var.project_id                # set the default GCP project to deploy resources into
+  region = var.region                              # sets defauly GCP region for resources
 }
