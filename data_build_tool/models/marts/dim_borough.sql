b80c4483ebdd6cbc5ba012893a5b3544bfa78ad1
XiaoFengMai <113124825+XiaoFengMai@users.noreply.github.com>
2026-03-16 13:22:29 -0400

    Update dim_borough.sql

--- data_build_tool/models/marts/dim_borough.sql
+++ data_build_tool/models/marts/dim_borough.sql
@@ -1,5 +1,5 @@
-# this query is used to ensure there are no typos for the borough names
+-- this query is used to ensure there are no typos for the borough names
 
-SELECT DISTINCT            # rmeove duplicates, select only unique values and return it once
-    borough                # select distinct using data from this column
-FROM {{ ref('stg_311_requests') }}    # current model depends on the staging model, which is built first
+SELECT DISTINCT            -- remove duplicates, select only unique values and return it once
+    borough                -- select distinct using data from this column
+FROM {{ ref('stg_311_requests') }}    -- current model depends on the staging model, which is built first
