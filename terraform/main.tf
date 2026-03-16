74278941b319517d954ba69b0fb1659e8d5f27bc
XiaoFengMai <113124825+XiaoFengMai@users.noreply.github.com>
2026-03-16 12:36:46 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,15 @@
-# create resources (google cloud storage bucket, bigquery dataset, etc)
+# create resources (data lake bucket cloud storage for raw data storage, bigquery dataset data warehouse for analytics)
 
-resource "google_storage_bucket" "data_lake_bucket" {
-  name          = var.gcs_bucket_name
-  location      = var.region
-  storage_class = "STANDARD"
+resource "google_storage_bucket" "data_lake_bucket" {            # declares a new GCS bucket resource with gsc as the resource type and data lake bucket is used as a local reference name 
+  name          = var.gcs_bucket_name                      # sets the actual bucket name in GCP *must be globally unqique*, pulled from a variable
+  location      = var.region                        # sets the geographic region where the bucket is be created
+  storage_class = "STANDARD"                  # sets the storage class to standard which is best for frequently accessed data
   force_destroy = true
 
-  uniform_bucket_level_access = true
+  uniform_bucket_level_access = true          # allows terraform to delete the bucket even if it still contains files
 
-  versioning {
-    enabled = true
+  versioning {                # opens a version configuration block for the bucket
+    enabled = true                    # turns on object versioning so GCP can keep a history of all file versions.
   }
 }
 
@@ -18,9 +18,9 @@ resource "google_storage_bucket" "data_lake_bucket" {
 
 # create data warehouse dataset in bigquery
 
-resource "google_bigquery_dataset" "dataset" {
-  dataset_id  = var.bq_dataset_name
-  location    = var.region
-  description = "NYC 311 analytics dataset"
+resource "google_bigquery_dataset" "dataset" {          # declares a new BigQuery dataset resource with google bq ds as resource type and dataset as local reference name
+  dataset_id  = var.bq_dataset_name                # sets the bigquery dataset id pulled from a variable
+  location    = var.region                # sets region using variable
+  description = "NYC 311 analytics dataset"        
 }
 
