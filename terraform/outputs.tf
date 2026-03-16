1ecfa0a5063a5ea8f3b326c1b448e43f0f75728c
XiaoFengMai <113124825+XiaoFengMai@users.noreply.github.com>
2026-03-16 10:30:42 -0400

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,11 @@
-# values that Terraform prints after deployment
+# values that Terraform prints after a successful terraform apply
 
-output "bucket_name" {
-  value = google_storage_bucket.data_lake_bucket.name
+output "bucket_name" {               # declares a output variable named bucket_name
+  value = google_storage_bucket.data_lake_bucket.name        # tells terraform to display the name attribute as the value
 }
 
-output "bigquery_dataset" {
-  value = google_bigquery_dataset.dataset.dataset_id
+output "bigquery_dataset" {          # declares a second output variable named bigquery_dataset
+  value = google_bigquery_dataset.dataset.dataset_id         # reads the dataset_id attribute as the value
 }
 
 
