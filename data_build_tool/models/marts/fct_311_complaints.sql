5013f7be19e52fbd3f42faec70c5fd8c03d3e5c4
XiaoFengMai <113124825+XiaoFengMai@users.noreply.github.com>
2026-03-16 13:22:47 -0400

    Update fct_311_complaints.sql

--- data_build_tool/models/marts/fct_311_complaints.sql
+++ data_build_tool/models/marts/fct_311_complaints.sql
@@ -1,8 +1,8 @@
 # this model shows how many complaints were filed for each uinque complaint type in the boroughs 
 
-SELECT                # initiates query
-    borough,                   # selects borough column
-    complaint_type,                # selects complaint category 
-    COUNT(*) AS complaint_count                # counts every row that has borough and complaints data and labels it complaint_count (*) -> regardless of null values
-FROM {{ ref('stg_311_requests') }}        # pulls data from stg_311_requests staging model using dbt's ref() function
-GROUP BY borough, complaint_type               # creates a row for every unique combinaition of borough and complaint type 
+SELECT                -- initiates query
+    borough,                   -- selects borough column
+    complaint_type,                -- selects complaint category 
+    COUNT(*) AS complaint_count                -- counts every row that has borough and complaints data and labels it complaint_count (*) -> regardless of null values
+FROM {{ ref('stg_311_requests') }}        -- pulls data from stg_311_requests staging model using dbt's ref() function
+GROUP BY borough, complaint_type               -- creates a row for every unique combinaition of borough and complaint type 
