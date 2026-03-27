ce4c9c59659acdb9cebfc8d40b2b3f3b0e263ef0
XiaoFengMai <113124825+XiaoFengMai@users.noreply.github.com>
2026-03-27 16:22:52 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,27 +2,29 @@
 
 # defines a variable named project_id, replace this with your unique google cloud project iD
 variable "project_id" {          
-  description = "project-bdcd6ccf-5abd-43e1-a88"
+  description = "The GCP Project ID"
   type        = string
 }
 
 
 # defines a variable named region, replace this with name of region, select the region that best works for you
 variable "region" {                
-  description = "GCP region"        
+  description = "The GCP region"        
   default     = "us-central1"      
 }
 
 
 # defines a variable named gcs_bucket_name, replace this with your unique bucket name 
 variable "gcs_bucket_name" {              
-  description = "nyc-311-gcs-data-lake" 
+  description = "GCS Bucket Name" 
   type        = string
 }
 
 
 # defines a variable named bigquery_dataset_name, replace this with your unique bigquery dataset name
 variable "bigquery_dataset_name" {             
-  description = "nyc_311_bigquery_dataset"       
+  description = "Big Query Dataset"       
   type        = string
 }
+
+
